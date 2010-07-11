579630d80142d8b195b2436ca53c84b505f566c7
Aaron Hamid <aaron.hamid@gmail.com>
2010-07-11 19:49:16 -0400

    cmake script always regenerates makefiles

--- cmakeit.sh
+++ cmakeit.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-[ ! -d build ] && (mkdir -p build && cd build && cmake ..)
+#[ ! -d build ] && (mkdir -p build && cd build && cmake ..)
+(mkdir -p build && cd build && cmake ..)
 cmake --build build --clean-first
